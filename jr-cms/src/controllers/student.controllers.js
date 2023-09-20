e3aa315d29c99b4cfe8a62300f0dd99b2f84ac93
Mason Xiong <mason.xiong@yahoo.com>
2023-09-20 22:45:34 +1000

    update notes

--- jr-cms/src/controllers/student.controllers.js
+++ jr-cms/src/controllers/student.controllers.js
@@ -1,6 +1,44 @@
 const Student = require('../models/student.model');
+const Course = require('../models/course.model');
+const NotFoundException = require('../exceptions/NotFoundException');
 
-const addStudent = async (req, res) => {
+/**
+ * Handle errors -> handle async errors
+ * 1. try catch -> async await
+ * try {
+ *    xxxxx
+ * } catch(e) {
+ *    // or handle it directly
+ *    next(e);
+ * }
+ *
+ * 2. .catch -> promise
+ * Student.find().exec().then().catch(e => next(e));
+ *
+ * 3. callback
+ * Student.find().exec((err, students)=>{
+ *    if (err) {
+ *       handle error
+ *       next(err);
+ *       return
+ *    }
+ * })
+ */
+
+// const catchAllErrors = (routeHandler) => {
+//   return async (req, res,next) => {
+//     try {
+//       await routeHandler(req, res, next);
+//     } catch(e) {
+//       next(e);
+//     }
+//   }
+// }
+
+// express-async-errors
+
+const addStudent = async (req, res, next) => {
+  // try {
   const { firstName, lastName, email } = req.body;
   // data validation
   const student = new Student({ firstName, lastName, email });
@@ -8,8 +46,10 @@ const addStudent = async (req, res) => {
   // try {
   await student.save();
   res.json(student);
-  // } catch(e) {
-
+  // } catch (e) {
+  //   console.log(e);
+  //   next(e);
+  //   // res.send(400);
   // }
 };
 const getAllStudents = async (req, res) => {
@@ -26,8 +66,11 @@ const getStudentById = async (req, res) => {
   const { id } = req.params;
   const student = await Student.findById(id).exec();
   if (!student) {
-    res.status(404).json({ error: 'Student not found' });
-    return;
+    // throw new Error("xxxxx");
+    throw new NotFoundException('Student not found');
+    // res.status(404).json({ error: 'Student not found' });
+    // return;
+    // next(new NotFoundException(xxxx))
   }
   res.json(student);
 };
@@ -58,6 +101,61 @@ const deleteStudentById = async (req, res) => {
     res.status(404).json({ error: 'Student not found' });
     return;
   }
+  await Course.updateMany(
+    {
+      students: student._id,
+    },
+    {
+      $pull: {
+        students: student._id,
+      },
+    }
+  ).exec();
+  res.sendStatus(204);
+};
+// POST /v1/students/:studentId/courses/:courseId
+const addStudentToCourse = async (req, res) => {
+  const { studentId, courseId } = req.params;
+
+  // 通过id找student和course
+  const student = await Student.findById(studentId).exec();
+  const course = await Course.findById(courseId).exec();
+  // 确保学生和课程确实存在
+  if (!student || !course) {
+    res.status(404).json({ error: 'Student or course not found' });
+    return;
+  }
+
+  // 把学生添加进课程
+  course.students.addToSet(studentId);
+  // 把课程添加给学生
+  student.courses.addToSet(courseId);
+
+  // 记得保存
+  await student.save();
+  await course.save();
+
+  res.json(student);
+};
+
+// DELETE /v1/students/:studentId/courses/:courseId
+const removeStudentFromCourse = async (req, res) => {
+  const { studentId, courseId } = req.params;
+
+  // 通过id找student和course
+  const student = await Student.findById(studentId).exec();
+  const course = await Course.findById(courseId).exec();
+  // 确保学生和课程确实存在
+  if (!student || !course) {
+    res.status(404).json({ error: 'Student or course not found' });
+    return;
+  }
+  student.courses.pull(courseId); // pull -> $pull
+  course.students.pull(studentId);
+
+  await student.save();
+  await course.save();
+
   res.sendStatus(204);
 };
 
@@ -67,4 +165,6 @@ module.exports = {
   getStudentById,
   updateStudentById,
   deleteStudentById,
+  addStudentToCourse,
+  removeStudentFromCourse,
 };
